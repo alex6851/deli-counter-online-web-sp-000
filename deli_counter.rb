bd680b49ff817fd145576ae13160fcb7f1d97d39
Alexander Baker <alex.baker685@gmail.com>
2018-11-26 22:38:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli,name)
-puts "Welcome #{name}. You are number #{index + 1} in line"
+  katz_deli << name
+puts "Welcome #{name}. You are number #{array.length} in line"
 end
